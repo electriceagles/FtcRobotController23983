712252e3e5c9af9c4f1f5321944e80b82ba03504
Tamzid Hasan <tamzidghost65@gmail.com>
2025-11-12 17:18:36 -0500

    trying basic drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicDrive.java
@@ -7,21 +7,23 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class basicDrive extends LinearOpMode {
     private DcMotor leftDrive;
     private DcMotor rightDrive;
+
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
 
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        if (gamepad1.left_stick_y < -0.1){
+        if (gamepad1.left_stick_y < -0.1) {
             leftDrive.setPower(0.6);
             rightDrive.setPower(0.6);
         } else {
             leftDrive.setPower(0);
             rightDrive.setPower(0);
         }
+    }
 }
 
 
