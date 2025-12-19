90c5345f4792aa6b77873362fdabb6fe718c5fb0
Tamzid Hasan <tamzidghost65@gmail.com>
2025-12-19 17:29:11 -0500

    test servo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servotest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "test servo", group = "TeleOp")
+public class servotest extends LinearOpMode {
+    public Servo servo;
+
+    @Override public void runOpMode() {
+        servo = hardwareMap.get(Servo.class, "servo");
+
+        //SERVO SYSTEM MUST BE TUNED LATER TO RIGHT POSITIONS
+        if (gamepad1.right_trigger > 0.1) {
+            servo.setPosition(0.5); // moves servo 90 degrees
+        } else {
+            servo.setPosition(0); // moves back to og position
+        }
+    }
+}
