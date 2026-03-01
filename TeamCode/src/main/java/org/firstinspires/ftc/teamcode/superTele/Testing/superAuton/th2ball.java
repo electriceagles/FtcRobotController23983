5f0872348448035898d8e582563dc64877097f11
tamzid235 <tamzidghost65@gmail.com>
2026-03-01 00:25:03 -0500

    auton updates, adjusted tele to match x = strafe and y = forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/superTele/Testing/superAuton/th2ball.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/superTele/Testing/superAuton/th2ball.java
@@ -7,73 +7,140 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
-import javax.lang.model.element.Name;
+@Autonomous(name = "6 ball")
+public class th2ball extends LinearOpMode {
 
-
-@Autonomous(name = "three ball")
-public class thball extends LinearOpMode {
     DcMotorEx lf, lr, rf, rr;
+    DcMotorEx turret;
 
     // Mechanisms
     DcMotorEx intake;
-    DcMotorEx turret;
     DcMotorEx shooter1, shooter2;
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        // Drive motors
         lf = hardwareMap.get(DcMotorEx.class, "lf");
         lr = hardwareMap.get(DcMotorEx.class, "lr");
         rf = hardwareMap.get(DcMotorEx.class, "rf");
         rr = hardwareMap.get(DcMotorEx.class, "rr");
+
+        turret = hardwareMap.get(DcMotorEx.class, "turret");
+
         lf.setDirection(DcMotorSimple.Direction.FORWARD);
         lr.setDirection(DcMotorSimple.Direction.FORWARD);
         rf.setDirection(DcMotorSimple.Direction.REVERSE);
         rr.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        // Intake
         intake = hardwareMap.get(DcMotorEx.class, "i");
 
+        // Shooters
         shooter1 = hardwareMap.get(DcMotorEx.class, "sf1");
         shooter2 = hardwareMap.get(DcMotorEx.class, "sf2");
+
         shooter1.setDirection(DcMotorSimple.Direction.REVERSE);
         shooter2.setDirection(DcMotorSimple.Direction.FORWARD);
+
         shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         shooter2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         PIDFCoefficients pidf = new PIDFCoefficients(111.401, 0, 0, 14.05);
         shooter1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidf);
         shooter2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidf);
 
         waitForStart();
 
-        while (opModeIsActive()) {
+        if (opModeIsActive()) {
+
+            // Drive forward
             lf.setPower(0.7);
             lr.setPower(0.7);
             rf.setPower(0.7);
             rr.setPower(0.7);
-
-            // You're going to be going forward for 6 seconds without stop
             sleep(500);
 
-            lf.setPower(0);
-            lr.setPower(0);
-            rf.setPower(0);
-            rr.setPower(0);
+            stopDrive();
 
+            // Spin up shooters
             shooter1.setVelocity(1160);
             shooter2.setVelocity(1160);
-
             sleep(3000);
 
+            // Run intake
             intake.setPower(-1);
-
             sleep(8000);
+
             shooter1.setVelocity(0);
             shooter2.setVelocity(0);
             intake.setPower(0);
 
-            sleep(25000);
+            // Turn ~45 degrees
+            lf.setPower(-0.5);
+            lr.setPower(-0.5);
+            rf.setPower(0.5);
+            rr.setPower(0.5);
+            sleep(140);
+
+            stopDrive();
+
+            //intake
 
 
+            lf.setPower(-0.7);
+            lr.setPower(-0.7);
+            rf.setPower(-0.7);
+            rr.setPower(-0.7);
+            sleep(4500);
+
+            stopDrive();
+
+            intake.setPower(-1);
+            sleep(2500);
+
+            // Drive backward with intake
+            lf.setPower(0.7);
+            lr.setPower(0.7);
+            rf.setPower(0.7);
+            rr.setPower(0.7);
+            sleep(4500);
+
+            stopDrive();
+
+            lf.setPower(0.5);
+            lr.setPower(0.5);
+            rf.setPower(-0.5);
+            rr.setPower(-0.5);
+            sleep(140);
+
+            stopDrive();
+
+
+
+            // Final shooter spin
+            shooter1.setVelocity(1160);
+            shooter2.setVelocity(1160);
+            sleep(3000);
+
+            // Run intake
+            intake.setPower(-1);
+            sleep(8000);
+
+            shooter1.setVelocity(0);
+            shooter2.setVelocity(0);
+            intake.setPower(0);
+
+            stopDrive();
+
+            sleep(10000);
         }
+    }
 
+    private void stopDrive() {
+        lf.setPower(0);
+        lr.setPower(0);
+        rf.setPower(0);
+        rr.setPower(0);
     }
 }
